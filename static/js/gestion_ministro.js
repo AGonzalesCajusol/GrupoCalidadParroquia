7fe9d8fe83f581592a0d85ad6b4fdffafe5cd598
AGonzalesCajusol <anggelo243120@gmail.com>
2024-10-09 09:00:08 -0500

    cambio ministro

--- static/js/gestion_ministro.js
+++ static/js/gestion_ministro.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    // Inicializar DataTable
-    var table = $('#ministrosTable').DataTable({
-        pageLength: 8, 
-        dom: '<"d-flex justify-content-between align-items-center mb-3"<"d-flex"f><"d-flex justify-content-end button-section">>rt<"bottom"p>',  // Utilizar "justify-content-end" para alinear a la derecha
-
-        //dom: '<"d-flex justify-content-between align-items-center mb-3"<"d-flex"l<"d-flex justify-content-between align-items-center"f<"ml-3 button-section">>>rt<"bottom"p>',
-        initComplete: function() {
-            // Insertar el botón "Agregar ministro" dentro del div y alinearlo a la derecha
-            $("div.button-section").html('<button type="button" class="btn btn-success btn-lg custom-btn ml-3 btn-agregar-ministro" data-bs-toggle="modal" onclick="openModal(\'add\')"><i class="bi bi-person-plus"></i> Agregar ministro</button>');
-        }
-    });
+$('#ministrosTable').DataTable({
+    pageLength: 8,
+    dom: '<"d-flex justify-content-between align-items-center mb-3"<"d-flex"f><"d-flex justify-content-end button-section">>rt<"bottom"p>',
+    language: {
+        search: "Buscar:"
+    },
+    initComplete: function() {
+        $("div.button-section").html('<button type="button" class="btn btn-success btn-lg custom-btn ml-3 btn-agregar-ministro" data-bs-toggle="modal" onclick="openModal(\'add\')"><i class="bi bi-person-plus"></i> Agregar ministro</button>');
+    }
 });
+
 
 
 // Función para abrir el modal para agregar, ver o editar un ministro
